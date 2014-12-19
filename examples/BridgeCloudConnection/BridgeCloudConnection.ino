e633e588f36d4d27682815a255ffd8925d6fa06b
Khaled Osman <khaled.osmaan@gmail.com>
2014-12-19 22:23:28 +0100

    code cleaning

--- examples/BridgeCloudConnection/BridgeCloudConnection.ino
+++ examples/BridgeCloudConnection/BridgeCloudConnection.ino
@@ -2,63 +2,50 @@
     *** Simple example that shows how to use the Wunderbar Bridge module
     *** to connect an Ardino to the relayr open sensor cloud.
 
-    The application uses Serial communication to send data from the Arduino to the bridge module then to the relayr open sensor cloud,
-    and receivs data from the cloud back to the bridge to the Arduino again. You can see the data sent/received through the debug serial monitor.
+    The application uses Serial communication to send data from the serial monitor to the Arduino then to the bridge module and finally to the relayr open sensor cloud,
+    and receivs data from the cloud back to the bridge to the Arduino again. You can see the data sent/received through the serial monitor and the developer dashboard.
  */
  
 #include <WunderbarBridge.h>
-#include <SoftwareSerial.h>
 
 /*  Config the bridge module with the baudrate,
     which has to be the same used in the PC serial monitor application*/
+	
 Bridge bridge = Bridge(115200);
 static bridge_payload_t rxPayload;
-static uint8_t dataout[2] = {1, 2};
-  
+static uint8_t dataout[1];
+
 void setup() {	  
   if(bridge.begin())
   Serial.print("Bridge Connected\n");
   else
   Serial.print("Bridge NotConnected\n");
   
-  //Set the port for debugging, default is 0
+  //Set the port for debugging, default is Serial0
   //bridge.setDebugPort(0);
-  //Set the port for bridge connection, default is 1
+  //Set the port for bridge connection, default is Serial1
   //bridge.setBridgePort(1);
 }
 
 /* Main Loop */
 void loop() {
   //when we get new data from the cloud
-  if (bridge.newData == true){		
+  if (bridge.newData){		
     Serial.println("Data Received!\n");  
     rxPayload = bridge.getData();	
     delay(100);
   }
+  
   //enter data in Serial monitor and send it to bridge
   if(Serial.available())
   {
-     char c =Serial.read();
-     Serial.println(c);
-     static uint8_t dataout2[1];
-     dataout2[0]=c;
-//     Serial1.write(c);
-     bridge.sendData(dataout2,sizeof(dataout2));
+     char c = Serial.read();
+     dataout[0] = c;
+     bridge.sendData(dataout,sizeof(dataout));
   }
+  
   if(Serial1.available())
   {
      bridge.processSerial();
   }
 }
-
-// Same as Serial1.available() 
-/*
-void serialevent1()
-{
-  Serial.print("hi");
-  bridge.processSerial();
-}*/
-
-
-
-
